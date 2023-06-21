b083af32af9209a6f1678b91f02187016d9125ca
Suman Nandi <isuman.nandi@gmail.com>
2023-06-21 11:54:30 +0530

    Initial Commit

---
+++ swiftui-mvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-mvvm
+//
+//  Created by Suman Nandi on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Suman Nandi on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
